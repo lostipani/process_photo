7cabf72d64ea370d624d357b0fd889a9ab50c546
lostipani <lorenzo.stipani@gmail.com>
2023-10-01 20:15:24 +0200

    implemented a parser

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
-Compilation steps and execution on Linux machine. Write your `main.cpp` accordingly to the input.
+## Dependencies
+[OpenCV](https://docs.opencv.org/4.8.0/d9/df8/tutorial_root.html)
+[Boost::program_options](https://www.boost.org/doc/libs/1_83_0/doc/html/program_options.html)    
 
+### Build Boost::program_options
+1. Untar and move the `boost_1_xx_x/` folder to `/usr/local/`
+1. Run bootstrap to prepare library building
+    ```
+    sudo ./bootstrap.sh --prefix=/usr/local --with-libraries=program_options
+    sudo ./b2 install
+    ls /usr/local/lib/libboost*
+    ls /usr/local/include/boost/
+    ```
+
+
+## Compilation and execution
+
+Compilation steps and execution on Linux machine. Write your `main.cpp` accordingly to the input.
 1. CMakeLists.txt with following:
     ```
     cmake_minimum_required(VERSION 2.8)
@@ -12,4 +28,4 @@ Compilation steps and execution on Linux machine. Write your `main.cpp` accordin
     ```
 1. `cmake [-DCMAKE_CXX_FLAGS="-g"] .`
 1. `make`
-1. `./process [args]`
+1. `./process --options [args]`
--- src/utils.cpp
+++ src/utils.cpp
@@ -9,7 +9,7 @@ exhibit::Data_t exhibit::utils::input(const std::string& path)
 void exhibit::utils::output(const exhibit::Data_t& src, const std::string& path)
 {
     if (!cv::imwrite(path, src)) 
-        std::cerr << "Saving failed\n";
+        std::cerr << "Saving failed" << std::endl;
 }
 
 exhibit::Scalar_t exhibit::utils::rgb2bgr(const std::vector<int>& rgb)
@@ -19,4 +19,91 @@ exhibit::Scalar_t exhibit::utils::rgb2bgr(const std::vector<int>& rgb)
     double r = static_cast<double>(rgb.at(0));
 
     return Scalar_t {b,g,r};
+}
+
+
+bool exhibit::utils::parser(int argc, char** argv, exhibit::Parameters& params, int& nrows, int& ncols, std::vector<std::string>& source_files, std::string& save_name)
+{
+    namespace po = boost::program_options;
+    
+    // declarations
+    bool force_quit = false; // =true in case caller should be terminated, e.g. --help
+    std::string config_file;
+    po::options_description generic("Cmd-line options");
+    po::options_description config("Parameters configuration");
+    po::options_description visible("Options");
+    po::options_description hidden("Hidden options");
+    po::options_description cmdline_options;
+    po::options_description config_file_options;
+    po::positional_options_description posit; // positional for source file(s)
+    po::variables_map vm;
+    
+    // specifications
+    generic.add_options()
+        ("help", "Produce help message")
+        ("config", po::value<std::string>(&config_file)->default_value("parameters.cfg"),"Parameters configuration file path")
+        ("save", po::value<std::string>(&save_name)->default_value("test.jpg"),"Save canvas in name.jpg/png")
+        ("cross", "Canvas Cross layout, overwrites choice of nrows/ncols")
+        ("nrows", po::value<int>(&nrows)->default_value(1), "Canvas Matrix layout, number of rows, int")
+        ("ncols", po::value<int>(&ncols)->default_value(1), "Canvas Matrix layout, number of columns, int")
+        ;
+
+
+    config.add_options()
+        ("canvasAspectRatio", po::value<float>(&params.canvasAspectRatio), "Canvas base/height, float")
+        ("frameAspectRatio", po::value<float>(&params.frameAspectRatio), "Frame base/height, always >1, float")
+        ("hspaceRatio", po::value<float>(&params.hspaceRatio), "Frames horizontal spacing / Frame base, float")
+        ("vspaceRatio", po::value<float>(&params.vspaceRatio), "Frames vertical spacing / Frame base, float")
+        ("frame2canvasRatio", po::value<float>(&params.frame2canvasRatio), "Frame base / Canvas base, float")
+        ("photo2frameRatio", po::value<float>(&params.photo2frameRatio), "Photo base / Frame base, float")
+        ("frameThickRatio", po::value<std::vector<float>>(&params.frameThickRatio)->multitoken(), "Frame thickness / base, 4 floats {top,bot,lft,rgt}")
+        ("canvasBase", po::value<int>(&params.canvasBase), "Canvas base, pixels")
+        ("canvasColor", po::value<std::vector<int>>(&params.canvasColor)->multitoken(), "RGB, 3 ints")
+        ("frameColor", po::value<std::vector<int>>(&params.frameColor)->multitoken(), "RGB, 3 ints")
+        ("passpartoutColor", po::value<std::vector<int>>(&params.passpartoutColor)->multitoken(), "RGB, 3 ints")
+        ;
+
+    posit.add("sourceFiles", -1);
+
+    hidden.add_options()
+        ("sourceFiles", po::value<std::vector<std::string>>(), "Source file(s) path")
+        ;
+    
+    cmdline_options.add(generic).add(config).add(hidden);
+    config_file_options.add(config).add(hidden);
+    visible.add(generic).add(config);
+
+
+    // executions
+    store(po::command_line_parser(argc, argv).options(cmdline_options).positional(posit).run(), vm);
+    notify(vm);
+    
+    if (vm.count("help")){
+        std::cout << "Mumi23 Process Photo Tool - October 2023" << std::endl << std::endl;
+        std::cout << "Usage: process [options] sources..." << std::endl << std::endl;
+        std::cout << visible << std::endl;
+        force_quit = true;
+        return force_quit;
+    }
+    
+    
+    std::ifstream ifs(config_file.c_str());
+    if (!ifs) {
+        std::cerr << "cannot open configuration file" << std::endl;
+        force_quit = true;
+        return force_quit;
+    }
+    else {
+        store(parse_config_file(ifs, config_file_options), vm);
+        notify(vm);
+        source_files = vm["sourceFiles"].as<std::vector<std::string>>();
+    }
+
+
+    if (vm.count("cross")) {
+        ncols = 0;
+        nrows = 0;
+    }
+
+    return force_quit;
 }
